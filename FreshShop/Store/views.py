a25714019b0ad78e46fe0b5ba584e389514f9143
Stefen <2317708584@qq.com>
2019-07-23 23:29:06 +0800

    2019.7.23完成店铺注册、添加展示商品

--- FreshShop/Store/views.py
+++ FreshShop/Store/views.py
@@ -1,6 +1,7 @@
 import hashlib
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
+from django.core.paginator import Paginator
 
 from Store.models import *
 
@@ -51,6 +52,7 @@ def login(request):
                 if user.password == web_password and cookies == "login_page":
                     response = HttpResponseRedirect("/Store/index/")
                     response.set_cookie("username", username)
+                    response.set_cookie("user_id", user.id)
                     request.session["username"] = username
                     return response
     return response
@@ -58,5 +60,104 @@ def login(request):
 
 @loginValid
 def index(request):
-    return render(request, "store/index.html")
+    user_id = request.COOKIES.get("user_id")
+    if user_id:
+        user_id = int(user_id)
+    else:
+        user_id = 0
+    store = Store.objects.filter(user_id=user_id).first()
+    if store:
+        is_store = 1
+    else:
+        is_store = 0
+    return render(request, "store/index.html", {"is_store": is_store})
+
+
+def base(request):
+    return render(request, "store/base.html")
+
+
+def register_store(request):
+    type_list = StoreType.objects.all()
+    if request.method == "POST":
+        post_data = request.POST
+        store_name = post_data.get("store_name")
+        store_address = post_data.get("store_address")
+        store_description = post_data.get("store_description")
+        store_phone = post_data.get("store_phone")
+        store_money = post_data.get("store_money")
+
+        user_id = int(request.COOKIES.get("user_id"))
+        type_lists = post_data.getlist("type")
+
+        store_logo = request.FILES.get("store_logo")
+
+        # print(post_data)
+        # print(request.FILES)
+
+        store = Store()
+        store.store_name = store_name
+        store.store_address = store_address
+        store.store_description = store_description
+        store.store_phone = store_phone
+        store.store_money = store_money
+        store.user_id = user_id
+        store.store_logo = store_logo
+        store.save()
+
+        for i in type_lists:
+            store_type = StoreType.objects.get(id=i)
+            store.type.add(store_type)
+        store.save()
+
+    return render(request, "store/register_store.html", locals())
+
+
+def add_goods(request):
+    if request.method == "POST":
+        goods_name = request.POST.get("goods_name")
+        goods_price = request.POST.get("goods_price")
+        goods_number = request.POST.get("goods_number")
+        goods_description = request.POST.get("goods_description")
+        goods_date = request.POST.get("goods_date")
+        goods_safeDate = request.POST.get("goods_safeDate")
+        goods_store = request.POST.get("goods_store")
+        goods_image = request.POST.get("goods_image")
+
+        goods = Goods()
+        goods.goods_name = goods_name
+        goods.goods_price = goods_price
+        goods.goods_number = goods_number
+        goods.goods_description = goods_description
+        goods.goods_date = goods_date
+        goods.goods_safeDate = goods_safeDate
+        # goods.goods_store = goods_store
+        goods.goods_image = goods_image
+        goods.save()
+
+        goods.store_id.add(
+            Store.objects.get(id=int(goods_store))
+        )
+        goods.save()
+        return HttpResponseRedirect("/Store/list_goods/")
+    return render(request, "store/add_goods.html")
+
+
+def list_goods(request):
+    keywords = request.GET.get("keywords", "")
+    page_num = request.GET.get("page_num", 1)
+    if keywords:
+        goods_list = Goods.objects.filter(goods_name__contains=keywords)
+    else:
+        goods_list = Goods.objects.all()
+
+    paginator = Paginator(goods_list, 2)
+    page = paginator.page(int(page_num))
+
+    page_range = paginator.page_range
+
+    return render(request, "store/list_goods.html", {"page":page, "page_range":page_range,"keywords":keywords})
+
+
+
 
